cf2017cfa6b479b24d8cced892c62983c729aefa
Dalton Kirk <daltonkirk5@gmail.com>
2022-02-01 22:18:32 +0000

    Changed comparison view

---
+++ src/components/StatRow.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="stat-row">
+    <div class="left-side">
+      <div
+        class="bar-fill"
+        :class="barFillClass(true)"
+        :style="{ width: getPercent(leftValue, true) + '%' }"
+      ></div>
+      <div class="bar-value">{{ leftValue }}</div>
+    </div>
+    <div class="right-side">
+      <div
+        class="bar-fill"
+        :class="barFillClass(false)"
+        :style="{ width: getPercent(rightValue, false) + '%' }"
+      ></div>
+      <div class="bar-value">{{ rightValue }}</div>
+    </div>
+    <div class="stat-row__label">
+      <div class="">{{ label }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["label", "left-value", "right-value", "max-value"],
+  computed: {
+    cleanedLeftValue() {
+      let leftVal = this.leftValue;
+      if (this.leftValue.toString().indexOf("x") != -1) {
+        const numbers = this.leftValue.split("x");
+        leftVal = numbers[0] * numbers[1];
+      }
+      return leftVal;
+    },
+    cleanedRightValue() {
+      let rightVal = this.rightValue;
+
+      if (this.rightValue.toString().indexOf("x") != -1) {
+        const numbers = this.rightValue.split("x");
+        rightVal = numbers[0] * numbers[1];
+      }
+      return rightVal;
+    },
+    betterSide() {
+      if (this.label == "Recoil") {
+        if (this.cleanedLeftValue < this.cleanedRightValue) {
+          return "left";
+        } else if (this.cleanedLeftValue === this.cleanedRightValue) {
+          return "none";
+        } else {
+          return "right";
+        }
+      }
+
+      if (this.cleanedLeftValue > this.cleanedRightValue) {
+        return "left";
+      } else if (this.cleanedLeftValue === this.cleanedRightValue) {
+        return "none";
+      } else {
+        return "right";
+      }
+    },
+  },
+  methods: {
+    barFillClass(left) {
+      if (left && this.betterSide === "left") {
+        return "better";
+      } else if (left && this.betterSide === "right") {
+        return "worse";
+      } else if (!left && this.betterSide === "left") {
+        return "worse";
+      } else if (!left && this.betterSide === "right") {
+        return "better";
+      } else {
+          return ""
+      }
+    },
+    getPercent(value, left) {
+      if (!left ) {
+        return (this.cleanedRightValue / this.maxValue) * 100;
+      }
+      if (left) {
+        return (this.cleanedLeftValue / this.maxValue) * 100;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+$green: rgba(145, 255, 145, 0.5);
+$red: rgba(255, 70, 41, 0.5);
+
+.stat-row {
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  height: 10px;
+  width: 100%;
+  position: relative;
+}
+
+.left-side,
+.right-side {
+  //   border: lightgray;
+  //   border-width: 1px;
+  //   border-style: solid;
+  display: flex;
+  flex-basis: 50%;
+  height: 100%;
+  position: relative;
+  width: 100%;
+}
+
+.left-side {
+  justify-content: end;
+}
+
+.right-side {
+  .bar-value {
+    position: absolute;
+    right: 5px;
+    left: unset;
+    top: 0;
+  }
+}
+
+.bar-value {
+  position: absolute;
+  left: 5px;
+  top: 0;
+}
+
+.bar-fill {
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  transition: width 1s;
+
+  &.better {
+    background-color: $green;
+  }
+
+  &.worse {
+    background-color: $red;
+  }
+}
+
+.stat-row__label {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+}
+</style>
--- src/views/CompareAmmo.vue
+++ src/views/CompareAmmo.vue
@@ -13,7 +13,7 @@
       ></BulletSelector>
     </div>
     <div>
-      <BulletAttributeRow
+      <!-- <BulletAttributeRow
         :bullet="bulletLeft"
         :bullet-to-compare="bulletRight"
         property="FleshDamage"
@@ -85,55 +85,209 @@
         :bullet-to-compare="bulletRight"
         property="Armor6"
         display-name="Armor 6"
-      ></BulletAttributeRow>
+      ></BulletAttributeRow> -->
     </div>
     <div v-if="!bulletLeft || !bulletRight">
       Select two ammo types to compare
+    </div>
+    <div>
+      <stat-row
+        :label="'Flesh damage'"
+        :left-value="bulletLeft.FleshDamage"
+        :right-value="bulletRight.FleshDamage"
+        :max-value="bestFleshDamage"
+      />
+      <stat-row
+        :label="'Penetration power'"
+        :left-value="bulletLeft.PenetrationPower"
+        :right-value="bulletRight.PenetrationPower"
+        :max-value="bestPenPower"
+      />
+      <stat-row
+        :label="'Armor damage'"
+        :left-value="bulletLeft.ArmorDamage"
+        :right-value="bulletRight.ArmorDamage"
+        :max-value="bestArmourDmg"
+      />
+      <stat-row
+        :label="'Accuracy'"
+        :left-value="bulletLeft.Accuracy"
+        :right-value="bulletRight.Accuracy"
+        :max-value="bestAcc"
+      />
+      <stat-row
+        :label="'Recoil'"
+        :left-value="bulletLeft.Recoil"
+        :right-value="bulletRight.Recoil"
+        :max-value="highestRecoil"
+      />
+      <stat-row
+        :label="'Fragmentation Chance'"
+        :left-value="bulletLeft.FragmentationChance"
+        :right-value="bulletRight.FragmentationChance"
+        :max-value="100"
+      />
+      <stat-row
+        :label="'Armour 1'"
+        :left-value="bulletLeft.Armor1"
+        :right-value="bulletRight.Armor1"
+        :max-value="6"
+      />
+      <stat-row
+        :label="'Armour 2'"
+        :left-value="bulletLeft.Armor2"
+        :right-value="bulletRight.Armor2"
+        :max-value="6"
+      />
+      <stat-row
+        :label="'Armour 3'"
+        :left-value="bulletLeft.Armor3"
+        :right-value="bulletRight.Armor3"
+        :max-value="6"
+      />
+      <stat-row
+        :label="'Armour 4'"
+        :left-value="bulletLeft.Armor4"
+        :right-value="bulletRight.Armor4"
+        :max-value="6"
+      />
+      <stat-row
+        :label="'Armour 5'"
+        :left-value="bulletLeft.Armor5"
+        :right-value="bulletRight.Armor5"
+        :max-value="6"
+      />
+      <stat-row
+        :label="'Armour 6'"
+        :left-value="bulletLeft.Armor6"
+        :right-value="bulletRight.Armor6"
+        :max-value="6"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import BulletSelector from '@/components/BulletSelector.vue';
-import bulletData from '@/data/bullet.js';
-import BulletAttributeRow from '@/components/BulletAttributeRow';
+import BulletSelector from "@/components/BulletSelector.vue";
+import bulletData from "@/data/bullet.js";
+//import BulletAttributeRow from "@/components/BulletAttributeRow";
+import StatRow from "../components/StatRow.vue";
 
 export default {
-  name: 'CompareAmmo',
+  name: "CompareAmmo",
   components: {
     BulletSelector,
-    BulletAttributeRow,
+    // BulletAttributeRow,
+    StatRow,
   },
 
   metaInfo: {
-    title: 'Compare Ammo',
-    titleTemplate: '%s - Tarkov Ammo',
+    title: "Compare Ammo",
+    titleTemplate: "%s - Tarkov Ammo",
     meta: [
-      { charset: 'utf-8' },
+      { charset: "utf-8" },
       {
-        name: 'description',
+        name: "description",
         content:
-          'Compare Tarkov ammo side by side and determine which ammo is better. View damage differences, accuracy differences, and more.',
+          "Compare Tarkov ammo side by side and determine which ammo is better. View damage differences, accuracy differences, and more.",
       },
     ],
   },
 
-  data: function() {
+  data: function () {
     return {
-      bulletLeft: null,
-      bulletRight: null,
+      bulletLeft: {
+        FleshDamage: 0,
+        PenetrationPower: 0,
+        ArmorDamage: 0,
+        Accuracy: 0,
+        Recoil: 0,
+        FragmentationChance: 0,
+        Armor1: 0,
+        Armor2: 0,
+        Armor3: 0,
+        Armor4: 0,
+        Armor5: 0,
+        Armor6: 0,
+      },
+      bulletRight: {
+        FleshDamage: 0,
+        PenetrationPower: 0,
+        ArmorDamage: 0,
+        Accuracy: 0,
+        Recoil: 0,
+        FragmentationChance: 0,
+        Armor1: 0,
+        Armor2: 0,
+        Armor3: 0,
+        Armor4: 0,
+        Armor5: 0,
+        Armor6: 0,
+      },
       bullets: [],
     };
   },
+  computed: {
+    bestFleshDamage() {
+      let bestFd = 0;
+      for (let index = 0; index < this.bullets.length; index++) {
+        const element = this.bullets[index];
+        if (element.FleshDamage > bestFd) {
+          bestFd = element.FleshDamage;
+        }
+      }
+      return bestFd;
+    },
+    bestPenPower() {
+      let bestPen = 0;
+      for (let index = 0; index < this.bullets.length; index++) {
+        const element = this.bullets[index];
+        if (element.PenetrationPower > bestPen) {
+          bestPen = element.PenetrationPower;
+        }
+      }
+      return bestPen;
+    },
+    bestArmourDmg() {
+      let armourDmg = 0;
+      for (let index = 0; index < this.bullets.length; index++) {
+        const element = this.bullets[index];
+        if (element.ArmorDamage > armourDmg) {
+          armourDmg = element.ArmorDamage;
+        }
+      }
+      return armourDmg;
+    },
+    bestAcc() {
+      let bestAcc = 0;
+      for (let index = 0; index < this.bullets.length; index++) {
+        const element = this.bullets[index];
+        if (element.Accuracy > bestAcc) {
+          bestAcc = element.Accuracy;
+        }
+      }
+      return bestAcc;
+    },
+    highestRecoil() {
+      let bestRec = 0;
+      for (let index = 0; index < this.bullets.length; index++) {
+        const element = this.bullets[index];
+        if (element.Recoil > bestRec) {
+          bestRec = element.Recoil;
+        }
+      }
+      return bestRec;
+    },
+  },
   methods: {
-    bulletChangeLeft: function(bullet) {
+    bulletChangeLeft: function (bullet) {
       this.bulletLeft = bullet;
     },
-    bulletChangeRight: function(bullet) {
+    bulletChangeRight: function (bullet) {
       this.bulletRight = bullet;
     },
   },
-  mounted: function() {
+  mounted: function () {
     this.bullets = bulletData().bullets;
   },
 };
